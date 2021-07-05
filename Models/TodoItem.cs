6efde941b46ce486b6b591d4657d4e1c36fa15ca
Kalleby Santos <kalleby.santos@stepahead.pt>
2021-07-05 18:21:28 +0100

    👌 IMPROVE: Defining TodoItem Data Types

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ReactApp
 {
     public class TodoItem
     {
+        [Key]
         public int Id { get; set; }
 
+        [Column()]
         public string Title { get; set; }
 
+        [Column()]
         public string Description { get; set; }
     }
 }
