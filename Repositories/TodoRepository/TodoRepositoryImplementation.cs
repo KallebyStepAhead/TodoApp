d5e80c244bcf88e4a50c27237c5c8f775153652b
Kalleby Santos <kalleby.santos@stepahead.pt>
2021-07-05 18:44:01 +0100

    📦 NEW: Creating TodoItemRepositoryImplementation

---
+++ Repositories/TodoRepository/TodoRepositoryImplementation.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using ReactApp.Models;
+
+namespace ReactApp.Repositories.TodoRepository
+{
+    public class TodoRepositoryImplementation : ITodoRepository
+    {
+        public void CreateTodoItem(TodoItem todoItem)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteTodoItem(TodoItem todoItem)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TodoItem GetTodoItem(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<TodoItem> GetTodoItems()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateTodoItem(TodoItem todoItem)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
